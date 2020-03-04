6c906b0205bf71a4d3bce5a880d144708d77b6db
swapnanildeb <swapnanildeb3@gmail.com>
2020-03-03 22:41:01 -0600

    read in json

--- main_file.py
+++ main_file.py
@@ -1,17 +1,21 @@
 from Extract_Method_Tools import methods_parse, tools_parse
 from ingredient_scrape import RecipeFetcher
 from ing_steps import fetch_and_pack
+import json
 
-print("Getting All the Recipes from given URL")
+print("Please input a recipe URL:")
 rf = RecipeFetcher()
 url = input()
 results = rf.search_recipes(url)
-print("results ", results)
 print("Search is Over..Thank you for waiting!")
 if not results:
     print("Ooops I think it is not some kind of food")
 else:
 	fetch_and_pack(results[0])
+	recipe = {}
+	with open('recipe.json', 'r') as f:
+	    recipe = json.load(f)
+	print(recipe)
     # meat_lasagna = rf.search_recipes('meat lasagna')[0]
     # res = rf.scrape_recipe(meat_lasagna)
     # # print("res ", res)
